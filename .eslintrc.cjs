5a72af7afb66de3638f3bff52f2281ea96f83d25
wasihun-code <wasihunageru7947@gmail.com>
2023-09-01 11:58:40 +0530

    Intial Setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope */
+function App() {
+  return (
+    <div className="mx-auto border-2 mt-2 p-2">
+      <h1 className="text-3xl text-red-300">WasTrak : Progress Tracker</h1>
+    </div>
+  );
+}
+
+export default App;
