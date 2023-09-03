30b0f2a5a90a521908523e4885e357e06a93b9d8
wasihun-code <wasihunageru7947@gmail.com>
2023-09-03 16:19:33 +0530

    Improved ProjectCardMin design to resemble figma design

--- src/components/LayoutComponents/ProjectCardMin.tsx
+++ src/components/LayoutComponents/ProjectCardMin.tsx
@@ -1,12 +1,8 @@
 /* eslint-disable react/react-in-jsx-scope */
-
 export default function ProjectCardMin() {
-  const style = {
-    width: '75%',
-  };
   return (
-    <div className="p-1 flex flex-col mx-auto">
-      <div className="m-4 p-4 border-2 rounded-3xl bg-slate-200 border-slate-600 ">
+    <div className="p-1 mx-auto">
+      <div className="m-4 p-4 border-2 rounded-3xl bg-slate-200 border-slate-600">
         <div className="flex flex-row gap-2 ml-5">
           <img
             src="/src/assets/img/testimonail.png"
@@ -14,20 +10,16 @@ export default function ProjectCardMin() {
             className="w-1/5 rounded-full overflow-hidden"
           />
           <div className="flex flex-col justify-around ml-5">
-            <h1 className="font-bold text-4xl text-left text-blue-600 ">
+            <h1 className="font-bold text-4xl text-left text-blue-600">
               Fullstack webdeveloper
             </h1>
             <p className="text-left text-3xl text-red-500">
               Due on 21 Sep 2022
             </p>
-
-            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-              <div
-                className="bg-red-600 h-2.5 rounded-full"
-                style={style}
-              ></div>
-            </div>
           </div>
+        </div>
+        <div className="w-full mt-4 bg-gray-200 rounded-full h-6 dark:bg-gray-700">
+          <div className="bg-red-600 h-6 rounded-full w-3/4"></div>
         </div>
       </div>
     </div>
